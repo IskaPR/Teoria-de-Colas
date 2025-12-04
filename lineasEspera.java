72dbcd6884bc127aa70f5881a7faeb6812202bb9
IskaPR <dani.enero2006.dp@gmail.com>
2025-12-03 21:12:25 -0600

    creacion de los casos

--- lineasEspera.java
+++ lineasEspera.java
@@ -10,6 +10,36 @@ public class lineasEspera {
         System.out.println("1. Modelo M/M/1");
         System.out.println("2. Modelo M/M/c");
         System.out.println("3. Modelo M/M/1/K");
-        System.out.println("4. Salir");
+        opcion = scanner.nextInt();
+        double lambda = 0, mu = 0;
+        int c = 0, K = 0;
+        switch (opcion) {
+            case 1:
+                System.out.println("Ha seleccionado el modelo M/M/1");
+                System.out.println("Ingrese la tasa de llegada (λ): ");
+                lambda = scanner.nextDouble();
+                System.out.println("Ingrese la tasa de servicio (μ): ");
+                mu = scanner.nextDouble();
+                break;
+            case 2:
+                System.out.println("Ha seleccionado el modelo M/M/c");
+                System.out.println("Ingrese la tasa de llegada (λ): ");
+                lambda = scanner.nextDouble();
+                System.out.println("Ingrese la tasa de servicio (μ): ");
+                mu = scanner.nextDouble();
+                System.out.println("Ingrese el numero de servidores (c): ");
+                c = scanner.nextInt();
+                break;
+            case 3:
+                System.out.println("Ha seleccionado el modelo M/M/1/K");
+                System.out.println("Ingrese la tasa de llegada (λ): ");
+                lambda = scanner.nextDouble();
+                System.out.println("Ingrese la tasa de servicio (μ): ");
+                mu = scanner.nextDouble();
+                System.out.println("Ingrese la capacidad del sistema (K): ");
+                K = scanner.nextInt();
+                break;
+        }
+
     }
 }
