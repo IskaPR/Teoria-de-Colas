c4a28966277bf43124acafa1138fa2b28d1bc605
IskaPR <dani.enero2006.dp@gmail.com>
2025-12-03 20:23:59 -0600

    Creacion del menu

--- lineasEspera.java
+++ lineasEspera.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class lineasEspera {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("\t Teoria de lineas de espera");
-        
+        System.out.println("---------------------------------------------------");
+        int opcion = 0;
+        System.out.println("Seleccione el modelo que desea calcular: ");
+        System.out.println("1. Modelo M/M/1");
+        System.out.println("2. Modelo M/M/c");
+        System.out.println("3. Modelo M/M/1/K");
+        System.out.println("4. Salir");
     }
 }
